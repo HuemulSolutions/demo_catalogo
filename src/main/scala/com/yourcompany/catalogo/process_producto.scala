c24e210e6e86adb2c709567c6cccae1bfa11007f
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-09-14 17:51:33 -0300

    compatibilidad version 2.0.1 huemul

--- src/main/scala/com/yourcompany/catalogo/process_producto.scala
+++ src/main/scala/com/yourcompany/catalogo/process_producto.scala
@@ -131,7 +131,7 @@ object process_producto_Migrar {
     var param = huemulBigDataGov.ReplaceWithParams("{{YYYY}}-{{MM}}-{{DD}}", param_ano, param_mes, param_dia, 0, 0, 0)
     
    val clase = new tbl_comun_producto(huemulBigDataGov, null)
-   clase.CopyToDest(param, "[[environment]]")
+   clase.copyToDest(param, "[[environment]]")
    huemulBigDataGov.close
  }
  
